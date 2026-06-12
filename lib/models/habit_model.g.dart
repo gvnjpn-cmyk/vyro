6fdab04d1e24a58a6ffff94798fb6b24426abe53
gvnjpn-cmyk <gvnjpn@gmail.com>
2026-06-12 12:47:22 +0700

    Upload: lib/models/habit_model.g.dart

---
+++ lib/models/habit_model.g.dart
@@ -0,0 +1,55 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'habit_model.dart';
+
+class HabitModelAdapter extends TypeAdapter<HabitModel> {
+  @override
+  final int typeId = 1;
+
+  @override
+  HabitModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return HabitModel(
+      id: fields[0] as String?,
+      name: fields[1] as String,
+      streak: fields[2] as int,
+      completionHistory: (fields[3] as List).cast<DateTime>(),
+      icon: fields[4] as String,
+      color: fields[5] as String,
+      createdAt: fields[6] as DateTime?,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, HabitModel obj) {
+    writer
+      ..writeByte(7)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.name)
+      ..writeByte(2)
+      ..write(obj.streak)
+      ..writeByte(3)
+      ..write(obj.completionHistory)
+      ..writeByte(4)
+      ..write(obj.icon)
+      ..writeByte(5)
+      ..write(obj.color)
+      ..writeByte(6)
+      ..write(obj.createdAt);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is HabitModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
